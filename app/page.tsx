df81b04bded9bcf87f67c5d8b75d37e16f490a84
AstralPhoebus <157814663+AstralPhoebus@users.noreply.github.com>
2025-07-07 15:14:52 +0200

    Update page.tsx
    Retirer hotjar

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-<!-- Hotjar Tracking Code for Site 6456699 (nom manquant) -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:6456699,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-
-
 "use client";
 import React, { useState } from 'react';
 import AdventureGame from '@/components/AdventureGame'; // Adjust import path
